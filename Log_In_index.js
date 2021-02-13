3dd906440f902e5f51980390d4ee7fd17431ccb0
Laurence <smith.e.laurence@gmail.com>
2021-02-12 23:08:48 -0500

    Login/Update _indexes

---
+++ Log_In_index.js
@@ -0,0 +1,43 @@
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient(); 
+
+exports.handler = async (event) => {
+    //Get User Id from the query param
+    console.log(event); 
+    console.log(db);
+    const requestUID = event.queryStringParameters['user_id']['password'];
+    // Get the item from the database
+    let params = {
+        TableName : 'user_profiles',
+        Key: {
+            "user_id": requestUID,
+            "password": requestUID
+        }
+    };
+    //Return the item json
+    let result = await db.get(params).promise();
+    
+    if (result.Item == null)
+    {
+        const response = {
+            statusCode: 404,
+            body: "Could not find user with given id",
+        };
+        return response;
+    }
+    //Else prettify result and send response
+    
+    const responseBody = {
+        "name": result.Item.user_id,
+        "pass": result.Item.password
+    };
+   
+    console.log(result);
+    
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(responseBody),
+    };
+    return response;
+};
